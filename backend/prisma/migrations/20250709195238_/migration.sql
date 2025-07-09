0f9b6695ef07357211df12bf59f77e49fdb8cd32
Hector Zambrano Serrano <hectorzs@meta.com>
2025-07-09 12:59:40 -0700

    Fixed database issues

--- backend/prisma/migrations/20250709195238_/migration.sql
+++ backend/prisma/migrations/20250709195238_/migration.sql
@@ -1,4 +1,31 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "user_id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30),
+    "longitude" DECIMAL(65,30),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "post_id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "urgency" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "creator_id" INTEGER NOT NULL,
+    "volunteer_id" INTEGER,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("post_id")
+);
+
+-- CreateTable
 CREATE TABLE "Chat" (
     "chat_id" SERIAL NOT NULL,
     "userOne_id" INTEGER NOT NULL,
@@ -19,7 +46,19 @@ CREATE TABLE "Message" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Chat_userOne_id_userTwo_id_key" ON "Chat"("userOne_id", "userTwo_id");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_creator_id_fkey" FOREIGN KEY ("creator_id") REFERENCES "User"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_volunteer_id_fkey" FOREIGN KEY ("volunteer_id") REFERENCES "User"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userOne_id_fkey" FOREIGN KEY ("userOne_id") REFERENCES "User"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
